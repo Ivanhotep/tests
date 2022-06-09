59a4acb929850d53beebb8384c2fa6146ed3892a
Ioann Bakal <ivanencence@gmail.com>
2022-06-09 11:30:40 +0300

    ArraysLesson

---
+++ src/test/java/tests/ArraysOper.java
@@ -0,0 +1,22 @@
+package tests;
+
+import java.util.Arrays;
+
+public class ArraysOper {
+
+    public static void main(String[] args) {
+
+        int[] intArray = {24, 340, 0, 34, 12, 10, 20};
+        Arrays.sort(intArray);
+        int maxNum = intArray[intArray.length - 1];
+        int minNum = intArray[intArray[0]];
+        int lengthArray = intArray.length;
+        System.out.println("Max number = " + maxNum);
+        System.out.println("Min number = " + minNum);
+        System.out.println("Array length =" + lengthArray);
+
+        String[] namesArray = {"Ann", "John"};
+        System.out.println(Arrays.toString(namesArray));
+    }
+
+}
